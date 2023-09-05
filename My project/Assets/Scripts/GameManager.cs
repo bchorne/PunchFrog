97bd0dd230c4edc674116b725c410cb20dc7a290
bchorne <ben.chris.horne@gmail.com>
2023-09-06 11:52:33 +1200

    Player faces nearest enemy

---
+++ My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<Enemy> enemies;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,59 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
 
     public Joystick joy;
 
     Vector2 movement;
 
+    public GameManager gm;
+
+    public Enemy target;
+
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         movement.x = joy.Horizontal;
         movement.y = joy.Vertical;
+
+        //Find Nearest enemy, set them to be our target
+        FindNearest();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); //Move according to input
+
+        if (target != null) //Face nearest enemy
+        {
+            Vector2 LookAt = new Vector2(target.transform.position.x, target.transform.position.y) - rb.position;
+            float angle = Mathf.Atan2(LookAt.y, LookAt.x) * Mathf.Rad2Deg + 90f;
+            rb.rotation = angle;
+        }
+    }
+
+    private void FindNearest()
+    {
+        Enemy nearest = null;
+        float dist = Mathf.Infinity;
+
+        foreach (Enemy enemy in gm.enemies)
+        {
+            Vector2 diff = new Vector2(enemy.transform.position.x, enemy.transform.position.y) - rb.position;
+
+            if (diff.sqrMagnitude < dist)
+            {
+                nearest = enemy;
+                dist = diff.sqrMagnitude;
+            }
+        }
+
+        target = nearest;
     }
 }
